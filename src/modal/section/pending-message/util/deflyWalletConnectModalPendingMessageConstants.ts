4d2b78635d043d8472d48b56824eed32219bd618
mkhiatarav <mkhitar@blockshake.io>
2022-12-02 18:42:29 +0400

    Update s3 url

--- src/modal/section/pending-message/util/deflyWalletConnectModalPendingMessageConstants.ts
+++ src/modal/section/pending-message/util/deflyWalletConnectModalPendingMessageConstants.ts
@@ -1,4 +1,4 @@
 const CONNECT_TIMEOUT_INTERVAL = 30000;
-const CONNECT_AUDIO_URL = "https://s3.amazonaws.com/wc.deflywallet.app/audio.mp3";
+const CONNECT_AUDIO_URL = "https://defly-walletconnect.s3.eu-central-1.amazonaws.com/audio.mp3";
 
 export {CONNECT_AUDIO_URL, CONNECT_TIMEOUT_INTERVAL};
